b302c97b81d51c07402c50ad4582b16c7d2ced69
SweetLemon-art <ridhoputrasholeh@gmail.com>
2026-08-21 01:26:52 +0700

    test: run bitmap memory tests under Robolectric

--- app/src/test/java/com/example/ImageProcessorMemoryTest.kt
+++ app/src/test/java/com/example/ImageProcessorMemoryTest.kt
@@ -7,7 +7,12 @@ import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNotSame
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34])
 class ImageProcessorMemoryTest {
 
     @Test
